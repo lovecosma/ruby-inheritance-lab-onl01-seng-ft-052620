df2e276d40bd7d3f6b89d967844e0aa996776687
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-14 21:04:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student < User
+
+def first_name=(name)
+  @first_name = name
+end
+
+def first_name
+  @first_name
+end
+
+def last_name=(last_name)
+@last_name = last_name
+end
+
+def last_name
+  @last_name
+end
+
+
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+attr_accessor :first_name, :last_name, :knowledge
+
+def initialize()
+  @knowledge = []
+end
+
+def learn(knowledge_string)
+  @knowledge_string = knowledge_string
+  knowledge << @knowledge_string
+end
+
+
+end
